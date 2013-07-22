f746c68bdf9c772904ea9a0d3e22b387e37424af
Supun Nakandala <supun.nakandala@gmail.com>
2013-07-23 00:44:05 +0530

    added jsoup lib

--- stc/src/stc/Main.java
+++ stc/src/stc/Main.java
@@ -1,15 +1,7 @@
 package stc;
 
-/**
- *
- * @author Supun Nakandala
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
 }
