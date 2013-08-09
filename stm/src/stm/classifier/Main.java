1bcc9dec4457b9a0d9a52e14e8d5aaab1f46d346
Supun Nakandala <supun.nakandala@gmail.com>
2013-08-09 21:33:12 +0530

    added

---
+++ stm/src/stm/classifier/Main.java
@@ -0,0 +1,8 @@
+package stm.classifier;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SupervisedClassfier supervisedClassfier = new SupervisedClassfier();
+    }
+}
